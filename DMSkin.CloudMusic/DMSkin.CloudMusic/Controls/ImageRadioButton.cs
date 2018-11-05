a7b58f1ad20a869c162891a5187f76691312e37b
Dream.Machine <944095635@qq.com>
2018-11-05 18:36:17 +0800

    完成主页面大体框架

---
+++ DMSkin.CloudMusic/DMSkin.CloudMusic/Controls/ImageRadioButton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DMSkin.CloudMusic.Controls
+{
+    public class ImageRadioButton:RadioButton
+    {
+        public Geometry Image
+        {
+            get { return (Geometry)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(Geometry), typeof(ImageRadioButton));
+
+
+
+        public double ImageWidth
+        {
+            get { return (double)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageWidthProperty =
+            DependencyProperty.Register("ImageWidth", typeof(double), typeof(ImageRadioButton),new PropertyMetadata(15.0));
+
+        public double ImageHeight
+        {
+            get { return (double)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageHeightProperty =
+            DependencyProperty.Register("ImageHeight", typeof(double), typeof(ImageRadioButton),new PropertyMetadata(15.0));
+
+    }
+}
---
+++ DMSkin.CloudMusic/DMSkin.CloudMusic/Converters/EnumToBooleanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace DMSkin.CloudMusic.Converters
+{
+    public class EnumToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null||value.ToString()!=parameter.ToString())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && value.Equals(true) ? parameter : Binding.DoNothing;
+        }
+    }
+}
